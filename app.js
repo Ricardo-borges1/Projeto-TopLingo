dad796bddb98eb58aebcf1d7cc1075df5eaef713
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2024-04-02 10:10:46 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,60 +1,183 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//   const chk = document.getElementById('chk');
+
+//   chk.addEventListener('change', () =>{
+//       document.body.classList.toggle('dark');
+//   });
+
+//   const texto = document.querySelector("#texto");
+//   const linguas = document.querySelectorAll("select");
+//   const traducao = document.querySelector("#traducao");
+//   const botao = document.querySelector("#botao");
+//   const speakBtn = document.querySelector('#speakBtn');
+
+//   const linguagens = {
+//       "en-GB": "Inglês",
+//       "pt-BR": "Português",
+//       "es-ES" : "Espanhol",
+//       "ru-RU": "Russo",
+//       "fr-Fr" : "Francês",
+//       "it-It" : "Italiano",
+//       "ja-Ja" : "Japonês",
+//       "Ar-Ar" : "Árabe"
+//   };
+
+//   linguas.forEach((select) => {
+//       for (let id in linguagens) {
+//           let escolhas = document.createElement("option");
+//           escolhas.value = id;
+//           escolhas.textContent = linguagens[id];
+//           if ((select.classList.contains("linguas") && id === "pt-BR") ||
+//               (select.classList.contains("linguas2") && id === "en-GB")) 
+//           {
+//               escolhas.selected = true;
+//           }
+//           select.appendChild(escolhas);
+//       }
+//   });
+
+//   // Validando o texto
+//   function traduzirTexto(texto) {
+//       if (texto) {
+//           traducaoApi(texto);
+//       } else {
+//           traducao.value = "";
+//       }
+//   }
+
+//   function traducaoApi(texto) {
+//       const lang1 = linguas[0].value;
+//       const lang2 = linguas[1].value;
+
+//       fetch(
+//           `https://api.mymemory.translated.net/get?q=${texto}&langpair=${lang1}|${lang2}`
+//       )
+//       .then((res) => res.json())
+//       .then((data) => {
+//           traducao.value = data.responseData.translatedText;
+//           falar(data.responseData.translatedText); // Chamando a função para falar o texto traduzido
+//       });
+//   }
+
+//   function falar(texto) {
+//       const voz = window.speechSynthesis;
+//       const utterance = new SpeechSynthesisUtterance(texto);
+//       voz.speak(utterance);
+//   }
+
+//   // Testando se o navegador suporta reconhecimento de voz
+//   if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+//       // Capturando a voz
+//       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+//       const recognition = new SpeechRecognition();
+
+//       // Iniciando reconhecimento automaticamente
+//     recognition.start();
+      
+//     let nataliaDetected = false;
+
+//       // Reconhecimento automático
+//       recognition.addEventListener('result', function(e) {
+//           console.log(e);
+//           const result = e.results[0][0].transcript;
+        
+//           // texto.value = result;
+//           console.log(result.trim().toUpperCase() );
+          
+//           // Verificando se "NATALIA" foi detectado
+//           if (result.trim().toUpperCase() === 'NATÁLIA') {
+//               nataliaDetected = true;
+//               recognition.stop()
+//               setTimeout(() => {
+//                 recognition.start(); // Continuar reconhecimento
+//                 texto.value = result;
+//               }, 2000);
+//           } else {
+//               // Se "NATALIA" não foi detectado e não foi previamente detectado, pare o reconhecimento
+//               if (!nataliaDetected) {
+//                   recognition.stop();
+//                   setTimeout(() => {
+//                     recognition.start(); // Continuar reconhecimento
+//                   }, 2000);
+//               }
+//               if (result.trim().toLowerCase() === 'alice') {
+//                   document.body.classList.toggle('luz-apagada');
+//               }
+//           }
+//       }, false);
+
+//       speakBtn.addEventListener('click', function(e) {
+//           recognition.start();
+//       });
+//   } else {
+//       alert('Este navegador não suporta esta funcionalidade ainda!');
+//   }
+
+//   botao.addEventListener('click', function() {
+//       traduzirTexto(texto.value);
+//   });
+
+// }, false);
+
+
 window.addEventListener('DOMContentLoaded', function() {
-  const chk = document.getElementById('chk')
+  const chk = document.getElementById('chk');
 
   chk.addEventListener('change', () =>{
-      document.body.classList.toggle('dark')
-  })
+      document.body.classList.toggle('dark');
+  });
 
   const texto = document.querySelector("#texto");
   const linguas = document.querySelectorAll("select");
   const traducao = document.querySelector("#traducao");
   const botao = document.querySelector("#botao");
+  const speakBtn = document.querySelector('#speakBtn');
 
   const linguagens = {
-    "en-GB": "Inglês",
-    "pt-BR": "Português",
-    "es-ES" : "Espanhol",
-    "ru-RU": "Russo",
-    "fr-Fr" : "Francês",
-    "it-It" : "Italiano",
-    "ja-Ja" : "Japonês",
-    "Ar-Ar" : "Árabe"
+      "en-GB": "Inglês",
+      "pt-BR": "Português",
+      "es-ES" : "Espanhol",
+      "ru-RU": "Russo",
+      "fr-Fr" : "Francês",
+      "it-It" : "Italiano",
+      "ja-Ja" : "Japonês",
+      "Ar-Ar" : "Árabe"
   };
 
   linguas.forEach((select) => {
-    for (let id in linguagens) {
-      let escolhas = document.createElement("option");
-      escolhas.value = id;
-      escolhas.textContent = linguagens[id];
-      if ((select.classList.contains("linguas") && id === "pt-BR") ||
-          (select.classList.contains("linguas2") && id === "en-GB")) 
+      for (let id in linguagens) {
+          let escolhas = document.createElement("option");
+          escolhas.value = id;
+          escolhas.textContent = linguagens[id];
+          if ((select.classList.contains("linguas") && id === "pt-BR") ||
+              (select.classList.contains("linguas2") && id === "en-GB")) 
           {
-        escolhas.selected = true;
+              escolhas.selected = true;
+          }
+          select.appendChild(escolhas);
       }
-      select.appendChild(escolhas);
-    }
   });
 
-  // validação de texto
-  function traduzirTexto(text) {
-    if (text) {
-      traducaoApi(text);
-    } else {
-      traducao.value = "";
-    }
+  // Validando o texto
+  function traduzirTexto(texto) {
+      if (texto) {
+          traducaoApi(texto);
+      } else {
+          traducao.value = "";
+      }
   }
 
-  function traducaoApi(text) {
-    const lang1 = linguas[0].value;
-    const lang2 = linguas[1].value;
-    
-    fetch(
-      `https://api.mymemory.translated.net/get?q=${text}&langpair=${lang1}|${lang2}`
-    )
+  function traducaoApi(texto) {
+      const lang1 = linguas[0].value;
+      const lang2 = linguas[1].value;
+
+      fetch(
+          `https://api.mymemory.translated.net/get?q=${texto}&langpair=${lang1}|${lang2}`
+      )
       .then((res) => res.json())
       .then((data) => {
-        traducao.value = data.responseData.translatedText;
-        falar(data.responseData.translatedText); // Chamando a função para falar o texto traduzido
+          traducao.value = data.responseData.translatedText;
+          falar(data.responseData.translatedText); // Chamando a função para falar o texto traduzido
       });
   }
 
@@ -64,38 +187,49 @@ window.addEventListener('DOMContentLoaded', function() {
       voz.speak(utterance);
   }
 
-  const speakBtn = document.querySelector('#speakBtn');
-
-  // testa se o navegador suporta o reconhecimento de voz
+  
+  // Testando se o navegador suporta reconhecimento de voz
   if (window.SpeechRecognition || window.webkitSpeechRecognition) {
-
-      // captura a voz
+      // Capturando a voz
       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
       const recognition = new SpeechRecognition();
 
-      // inicia reconhecimento
+      // Iniciando reconhecimento automaticamente
+      recognition.start();
+      
+      let nataliaDetected = false;
+
+      // Reconhecimento automático
+      recognition.addEventListener('result', function(e) {
+          const result = e.results[0][0].transcript;
+          
+          // Verificando se "NATALIA" foi detectado
+          if (result.trim().toUpperCase() === 'NATÁLIA') {
+              nataliaDetected = true;
+              recognition.stop();
+              setTimeout(() => {
+                  recognition.start(); // Continuar reconhecimento
+              }, 1000);
+          } else if (nataliaDetected) {
+              texto.value = result;
+          }
+      });
+
       speakBtn.addEventListener('click', function(e) {
           recognition.start();
-  
-      }, false);
+      });
 
-      // resultado do reconhecimento
-      recognition.addEventListener('result', function(e) {
-          console.log(e);
-          const result = e.results[0][0].transcript;
-          texto.value = result;
-          if (result.toLowerCase() === 'alice') {
-              document.body.classList.toggle('luz-apagada');
-          } else {
-          }
-      }, false);
+      recognition.stop()
+      setTimeout(() => {
+        recognition.start()
+      }, 500);
   } else {
       alert('Este navegador não suporta esta funcionalidade ainda!');
   }
 
- 
   botao.addEventListener('click', function() {
       traduzirTexto(texto.value);
   });
 
-}, false);
+});
+
